76e34be645a45bc781619f8e872bc5bc4b75f6ad
Oğuzhan Güngör Sönmez <arcadiacommunity36@gmail.com>
2026-05-04 14:34:17 +0300

    Db ve Kurulum Dosyaları eklendi

---
+++ GameQuest/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameQuest.Models;
+
+public partial class Game
+{
+    public int Id { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? ImageUrl { get; set; }
+
+    public virtual ICollection<Advert> Adverts { get; set; } = new List<Advert>();
+
+    public virtual Category Category { get; set; } = null!;
+}
---
+++ GameQuest/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameQuest.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? DiscordProfile { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public virtual ICollection<Advert> Adverts { get; set; } = new List<Advert>();
+
+    public virtual ICollection<Application> Applications { get; set; } = new List<Application>();
+}
